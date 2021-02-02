b29255a29b44bb41cbce14bd8c346b6ff0b7d5f8
james <jscampbell.05@googlemail.com>
2021-02-02 10:55:35 +0000

    Export report generator.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const { performance } = require('./performance')
 const { measure } = require('./measure')
 const Stopwatch = require('./stopwatch')
+const ReportGenerator = require('./report-generator')
 
 module.exports = {
     measure: measure,
     performance: performance,
-    Stopwatch: Stopwatch
+    Stopwatch: Stopwatch,
+    ReportGenerator: ReportGenerator
 }
