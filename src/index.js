393727ba9df7b92def6f916f9a4c2d385e280315
james <jscampbell.05@googlemail.com>
2021-02-02 10:39:48 +0000

    Export Stopwatch.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-const { fs } = require('fs')
 const { performance } = require('./performance')
 const { measure } = require('./measure')
-const { error } = require('console')
+const { Stopwatch } = require('./stopwatch')
 
 module.exports = {
     measure: measure,
-    performance: performance
+    performance: performance,
+    Stopwatch: Stopwatch
 }
